733fe8ff719f230eca6d8a5c15add0bbc0bfe8c5
Rohit <rohityadav787898@gmail.com>
2023-07-29 21:31:16 +0545

    learnt simple introduction on next.js from https://www.youtube.com/watch?v=NgayZAuTgwM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,30 @@
+import Link from "next/link"
+import { prisma } from "./db"
+import { todo } from "node:test"
+import { TodoItem } from "@/components/TodoItem"
 
-export default function Home(){
-  return <h1>Hi</h1>
+function getTodos(){
+  return prisma.todo.findMany()
+
+}
+
+async function toggleTodo(id:string,complete:boolean) {
+  "use server"
+
+  await prisma.todo.update({where:{id},data:{complete}})  
+}
+export default async function Home(){
+   const todos = await getTodos()
+  return <>
+  <header className="flex justify-between items-center mb-4 ">
+    <h1 className="text-2xl">Todos</h1>
+    <Link className="border border-slate-300 text-slate-300 px-2-py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href='/new'>New</Link>
+
+  </header>
+  <ul className="pl-4">
+    {todos.map(todo => (
+      <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+    ))}
+  </ul>
+  </>
 }
